366a9c976554c8c9ca14556a42ca2a2e4e093cee
vergellif <fvergelli@ascentio.com.ar>
2025-05-25 20:50:18 -0300

    se modifica algo

--- run_pytest_suite.sh
+++ run_pytest_suite.sh
@@ -4,5 +4,5 @@ TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 REPORT_FILE="results/pytest_report_${TIMESTAMP}.html"
 pytest API/suite_pytest/ \
   --html="${REPORT_FILE}" \
-  --self-contained-html \
+  --self-contained-html
 echo "Reporte generado: ${REPORT_FILE}"
